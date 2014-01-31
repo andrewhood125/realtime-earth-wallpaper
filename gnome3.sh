df2226a00f226abb46dda63a552a5ed139ccb957
Andrew Hood <andrewhood125@gmail.com>
2014-01-31 16:36:02 -0600

    gnome3.sh can now be updated successfully when run from cron

--- gnome3.sh
+++ gnome3.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
+PID=$(pgrep gnome-session)
+export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
 rm ~/Pictures/wallpaper/world_sunlight_map_rectangular.*
 TIME=`date +%s`
 echo $TIME
