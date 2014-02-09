3d029b15474a9b6b75b410edb203bd5725013ff7
Andrew Hood <andrewhood125@gmail.com>
2014-02-09 15:55:27 -0600

    Old wallpapers not being deleted bug - fixed.

--- mac.sh
+++ mac.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-rm ~/Pictures/wallpaper/World_sunlight_map_rectangular.*
+rm ~/Pictures/wallpaper/world_sunlight_map_rectangular.*
 wget http://www.opentopia.com/images/data/sunlight/world_sunlight_map_rectangular.jpg -O ~/Pictures/wallpaper/world_sunlight_map_rectangular.`date +%s`.jpg
